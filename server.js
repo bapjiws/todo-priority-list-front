277fab87c65d8df660496a8457cee916e1ea4287
bapjiws <alex.boklin@gmail.com>
2017-06-29 18:36:00 +0300

    Fix middleware usage in development

--- server.js
+++ server.js
@@ -6,27 +6,39 @@ const app = express();
 const inProductionMode = process.env.NODE_ENV === 'production';
 
 if (!inProductionMode) {
-    // Webpack Hot Middleware: https://github.com/glenjamin/webpack-hot-middleware
-    // Webpack Dev Middleware: https://github.com/webpack/webpack-dev-middleware
     const webpackConfig = require('./webpack.config');
     const compiler = require('webpack')(webpackConfig);
 
-    app.use(require('webpack-dev-middleware')(compiler, {
+    // Webpack Dev Middleware: https://github.com/webpack/webpack-dev-middleware
+    const webpackDevMiddleware = require('webpack-dev-middleware')(compiler, {
         noInfo: true, publicPath: webpackConfig.output.publicPath
-    }));
+    });
+    // Webpack Hot Middleware: https://github.com/glenjamin/webpack-hot-middleware
+    const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
 
-    app.use(require("webpack-hot-middleware")(compiler));
+    app.use(webpackDevMiddleware);
+    app.use(webpackHotMiddleware);
+
+    const bundlePath = path.join(__dirname, './build/index.html');
+    app.get('/tasks', (req, res) =>  {
+        res.write(webpackDevMiddleware.fileSystem.readFileSync(bundlePath));
+        res.end();
+    });
+
+    app.get('*', (req, res) => {
+        res.send('NOT HERE');
+    });
+} else {
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    app.get('/tasks', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build/index.html'));
+    });
+
+    app.get('*', (req, res) => {
+        res.send('NOT HERE');
+    });
 }
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/tasks', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
-});
-
-app.get('*', (req, res) => {
-    res.send('404');
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => inProductionMode ?
