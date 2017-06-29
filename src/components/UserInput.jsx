ab82bec2348f0bb549633a7a681455bcfa7cd707
bapjiws <alex.boklin@gmail.com>
2017-06-29 22:42:23 +0300

    Add todo to redux store on form submission

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,35 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { addTodo } from '../../redux/actions/todos';
 
-let todoStub = {
-    priority: 4,
-    name: 'Покрасить забор',
-    description: 'Покрасить забор вокруг дома розовым цветом до полуночи сегодня.'
-};
+// let todoStub = {
+//     priority: 4,
+//     name: 'Покрасить забор',
+//     description: 'Покрасить забор вокруг дома розовым цветом до полуночи сегодня.'
+// };
 
-export const UserInput = (props) => {
-    return <form className="form-container">
-    <span className="input-row">
-        <span>Priority:</span>
-        <select>
-            {
-                [1,2,3,4,5].map((priority, idx) => <option key={idx} value={priority}>{priority}</option>)
-            }
-        </select>
-    </span>
-        <span className="input-row">
-        <span>Name:</span>
-        <input type="text" />
-    </span>
-        <span className="input-row">
-        <span>Description:</span>
-        <input type="text" />
-    </span>
+class UserInput extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            priority: 0,
+            name: '',
+            description: ''
+        };
 
-        <button onClick={event => {event.preventDefault(); props.addTodo(todoStub)}}>Add</button>
-    </form>
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleInputChange(event) {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleClick(event) {
+        event.preventDefault();
+        this.props.addTodo(this.state);
+    }
+
+    render() {
+        return <form className="form-container">
+            <span className="input-row">
+                <span>Priority:</span>
+                <input
+                    type="number"
+                    name="priority"
+                    value={this.state.priority}
+                    onChange={this.handleInputChange}
+                />
+            </span>
+            <span className="input-row">
+                <span>Name:</span>
+                <input
+                    type="text"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleInputChange}
+                />
+            </span>
+            <span className="input-row">
+                <span>Description:</span>
+                <input
+                    type="text"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.handleInputChange}
+                />
+            </span>
+            <button onClick={this.handleClick}>Add</button>
+        </form>
+    }
 };
 
 export default connect(
